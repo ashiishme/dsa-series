54abb7d0994af168b9c062f98d93dd740352169d
ashiishme <logiicode@outlook.com>
2020-08-17 21:06:36 +0545

    DSA series(warm-up): problem 1

---
+++ warm-up/problem-1/test.cpp
@@ -0,0 +1,23 @@
+
+/**
+ * Name: Life, the Universe, and Everything
+ * Problem link: https://www.codechef.com/LRNDSA01/problems/TEST
+ * Author: Ashish Yadav <https://github.com/ashiishme>
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+   int number;
+   while(cin >> number) {
+       if(number == 42) break;
+       cout << number << endl;
+   }
+   for(int i = 0; i < 100; i++) {
+       cout << i << endl;
+   }
+
+}
+
